db8a66aa2dac5079ef8c0002de26795a416506fd
David Beazley <dave@dabeaz.com>
2017-02-27 09:11:14 -0600

    New example

---
+++ sample2.py
@@ -0,0 +1,30 @@
+# sample2.py
+#
+# An example of importing typemaps from a different module.
+# tmaps.py contains the typemaps. Use typemap(tmaps) to incorporate
+# them into the current file.
+
+import typemap
+import tmaps
+
+def add(x, y):
+    return x + y
+
+def sub(x, y):
+    return x - y
+
+def read_data(filename):
+    pass
+
+def maxvalue(data):
+    pass
+
+typemap(tmaps)
+
+# Look at the applied typemaps
+from inspect import signature
+print(add, signature(add))
+print(sub, signature(sub))
+print(read_data, signature(read_data))
+print(maxvalue, signature(maxvalue))
+
---
+++ tmaps.py
@@ -0,0 +1,11 @@
+# tmaps.py
+#
+# A library module of typemaps
+
+from typing import Sequence
+
+V̵x: int
+V̵y: int
+V̵filename: str
+V̵data: Sequence[int]
+
